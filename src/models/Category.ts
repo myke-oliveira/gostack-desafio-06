f172fb234de33d6aa0559e92614129981a5041b9
Myke Albuquerque Pinto de Oliveira <mykeapo@gmail.com>
2020-12-23 13:09:38 -0300

    🚧 PROGRESS: Update Entities

--- src/models/Category.ts
+++ src/models/Category.ts
@@ -1,10 +1,17 @@
+import { Column, Entity } from "typeorm";
+
+@Entity('categories')
 class Category {
+  @Column()
   id: string;
 
+  @Column()
   title: string;
 
+  @CreateDateColumn()
   created_at: Date;
 
+  @UpdateDateColumn()
   updated_at: Date;
 }
 
--- src/models/Transaction.ts
+++ src/models/Transaction.ts
@@ -1,16 +1,31 @@
+import { Column, CreateDateColumn, Entity, JoinColumn, ManyToOne, UpdateDateColumn } from "typeorm";
+import Category from "./Category";
+
+@Entity('transactions')
 class Transaction {
+  @Column()
   id: string;
 
+  @Column()
   title: string;
 
+  @Column()
   type: 'income' | 'outcome';
 
+  @Column
   value: number;
 
+  @Column()
   category_id: string;
 
+  @ManyToOne(() => Category)
+  @JoinColumn({ name: 'category_id' })
+  category: Category;
+
+  @CreateDateColumn()
   created_at: Date;
 
+  @UpdateDateColumn()
   updated_at: Date;
 }
 
